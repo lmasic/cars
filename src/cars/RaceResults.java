72b35bca2676ecbe4baa6d28b1d271b1d09d1038
virtuoso <virtuoso@192.168.0.213>
2017-08-22 18:55:14 +0200

    řazení

--- src/cars/RaceResults.java
+++ src/cars/RaceResults.java
@@ -11,7 +11,7 @@ import java.util.Objects;
  *
  * @author virtuoso
  */
-public class RaceResults {
+public class RaceResults implements Comparable{
 
     private Car car;
     private TimeSpan timeSpan;
